96c2ec46b077e514206f86844f949947784c7a47
Andrej Rode <mail@andrejro.de>
2016-02-07 01:02:35 +0100

    minutebear: models: enhance subject model

---
+++ README.md
@@ -0,0 +1,3 @@
+# fse-protokollsys
+
+Manage scanned minutes of exams and archived written exams
--- fse_projects/minutebear/models.py
+++ fse_projects/minutebear/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class Document(models.Model):
-    DOCUMENT_TYPES = (('w','written'),('o','oral'),('x','extra'))
+    DOCUMENT_TYPES = (('w','written'),
+                      ('m','written (memorization)'),
+                      ('o','oral'),
+                      ('s','summary'),
+                      ('x','extra'))
     id = models.AutoField(primary_key=True)
     subject = models.ForeignKey('Subject', on_delete=models.PROTECT)
     date = models.DateField()
@@ -10,8 +14,17 @@ class Document(models.Model):
 
 class Subject(models.Model):
     name = models.CharField(max_length=100)
+    abbrev = models.CharField(max_length=10)
+    names = models.ManyToManyField('self',
+                                   through='SubjectRename',
+                                   symmetrical = False)
 
 class Examiner(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     subject = models.ManyToManyField(Subject)
+
+class SubjectRename(models.Model):
+    current = models.ForeignKey(Subject, related_name='current')
+    previous = models.ForeignKey(Subject, related_name='previous')
+    year = models.DateField()
